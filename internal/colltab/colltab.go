aa6e509d01e8129428a258f982e39a090ea1a9ea
Marcel van Lohuizen <mpvl@golang.org>
2015-02-11 14:59:03 +0100

    text/internal/colltab: added package commment
    Change-Id: I6963165a20ac33bf01de01dd38d785bc1d49eb29
    Reviewed-on: https://go-review.googlesource.com/4551
    Reviewed-by: Rob Pike <r@golang.org>

---
+++ internal/colltab/colltab.go
@@ -0,0 +1,7 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package colltab contains functionality related to collation tables.
+// It is only to be used by the collate and search packages.
+package colltab
