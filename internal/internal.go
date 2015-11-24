954b8aef05fd17d496facedece773d68de6e3e26
Marcel van Lohuizen <mpvl@golang.org>
2015-11-24 14:49:48 +0100

    internal: add SortTags and UniqueTags
    Change-Id: Ic62776aa053585f0dcf25caae1de3c1f0333f81b
    Reviewed-on: https://go-review.googlesource.com/17186
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- internal/internal.go
+++ internal/internal.go
@@ -7,3 +7,45 @@
 // Package internal contains non-exported functionality that are used by
 // packages in the text repository.
 package internal // import "golang.org/x/text/internal"
+
+import (
+	"sort"
+
+	"golang.org/x/text/language"
+)
+
+// SortTags sorts tags in place.
+func SortTags(tags []language.Tag) {
+	sort.Sort(sorter(tags))
+}
+
+type sorter []language.Tag
+
+func (s sorter) Len() int {
+	return len(s)
+}
+
+func (s sorter) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sorter) Less(i, j int) bool {
+	return s[i].String() < s[j].String()
+}
+
+// UniqueTags sorts and filters duplicate tags in place and returns a slice with
+// only unique tags.
+func UniqueTags(tags []language.Tag) []language.Tag {
+	SortTags(tags)
+	if len(tags) == 0 {
+		return tags
+	}
+	k := 0
+	for i := 1; i < len(tags); i++ {
+		if tags[k].String() < tags[i].String() {
+			k++
+			tags[k] = tags[i]
+		}
+	}
+	return tags[:k+1]
+}
