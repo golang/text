ce34b78e21059b513a51202bb8d696d6341de000
Marcel van Lohuizen <mpvl@golang.org>
2015-12-17 22:16:20 +0100

    display: remove completely
    nice to have, but the warning on the build dashboard is a bit pesky.
    
    Change-Id: I934b456d6f0ad777542d2c79ed78a36d3e56506d
    Reviewed-on: https://go-review.googlesource.com/17967
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- display/display.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2014 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// NOTICE: Package display has moved to golang.org/x/text/language/display.
-// Use golang.org/x/text/language/display instead.
-package display // import "golang.org/x/text/language/display"
