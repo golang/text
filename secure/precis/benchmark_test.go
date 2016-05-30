eadef97772d04b62b52fcc293ec52e51e8f6cd3d
Marcel van Lohuizen <mpvl@golang.org>
2016-05-30 10:41:47 +0200

    secure/precis: add more benchmarks
    Caveat: only works for Go1.7
    
    Change-Id: I018a40ceb887789e3f8df99678d39a7eafca2f70
    Reviewed-on: https://go-review.googlesource.com/23598
    Reviewed-by: Nigel Tao <nigeltao@golang.org>
    Run-TryBot: Marcel van Lohuizen <mpvl@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>

--- secure/precis/benchmark_test.go
+++ secure/precis/benchmark_test.go
@@ -2,32 +2,54 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build go1.7
+
 package precis
 
-import (
-	"testing"
-)
+import "testing"
 
-func BenchmarkUsernameCaseMapped(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		UsernameCaseMapped.String("Malvolio")
+var benchData = []struct{ name, str string }{
+	{"ASCII", "Malvolio"},
+	{"Arabic", "دبي"},
+	{"Hangul", "동일조건변경허락"},
+}
+
+func doBench(b *testing.B, f func(b *testing.B, p *Profile, s string)) {
+	for _, tc := range testCases {
+		for _, d := range benchData {
+			b.Run(tc.name+"/"+d.name, func(b *testing.B) {
+				f(b, tc.p, d.str)
+			})
+		}
 	}
 }
 
-func BenchmarkUsernameCasePreserved(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		UsernameCasePreserved.String("Malvolio")
-	}
+func BenchmarkString(b *testing.B) {
+	doBench(b, func(b *testing.B, p *Profile, s string) {
+		for i := 0; i < b.N; i++ {
+			p.String(s)
+		}
+	})
 }
 
-func BenchmarkOpaqueString(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		OpaqueString.String("Malvolio")
-	}
+func BenchmarkBytes(b *testing.B) {
+	doBench(b, func(b *testing.B, p *Profile, s string) {
+		src := []byte(s)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			p.Bytes(src)
+		}
+	})
 }
 
-func BenchmarkNickname(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Nickname.String("Malvolio")
-	}
+func BenchmarkTransform(b *testing.B) {
+	doBench(b, func(b *testing.B, p *Profile, s string) {
+		src := []byte(s)
+		dst := make([]byte, 2*len(s))
+		t := p.NewTransformer()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			t.Transform(dst, src, true)
+		}
+	})
 }
