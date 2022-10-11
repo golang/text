369c86ba1108cfc8744033afe107422a2ad76611
cui fliter <imcusg@gmail.com>
2022-10-11 17:55:29 +0800

    all: fix a few function names on comments
    Change-Id: Ibdf787b7a62e8201393f2a4487cd8c67d8b0c91d
    Reviewed-on: https://go-review.googlesource.com/c/text/+/441717
    Run-TryBot: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Joedian Reid <joedian@golang.org>

--- unicode/cldr/collate.go
+++ unicode/cldr/collate.go
@@ -229,7 +229,7 @@ func skipSpace(s string) string {
 	return strings.TrimLeftFunc(s, unicode.IsSpace)
 }
 
-// consumes returns whether the next byte is ch. If so, it gobbles it by
+// consume returns whether the next byte is ch. If so, it gobbles it by
 // updating s.
 func consume(s *string, ch byte) (ok bool) {
 	if *s == "" || (*s)[0] != ch {
