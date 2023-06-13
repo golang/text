e3c038a67ff8a7b728cf9a527ca4d14ff7540536
Russ Cox <rsc@golang.org>
2023-06-13 13:46:27 -0400

    all: prepare for Unicode 15.0.0
    internal/gen has never been updated for Go 1.17's //go:build tags.
    Do that, and add the entry for Unicode 15.0.0.
    
    Unicode 15.0.0 will change 9fff to be a CJK ideograph, which will
    break the unicode/runenames example test.
    Delete that part of the example.
    
    For golang/go#55079.
    
    Change-Id: I12b0c6d031d6d6e1970c8fe6eb5453fa64c9f365
    Reviewed-on: https://go-review.googlesource.com/c/text/+/503035
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: David Chase <drchase@google.com>
    Auto-Submit: Russ Cox <rsc@golang.org>

--- internal/gen/gen.go
+++ internal/gen/gen.go
@@ -87,7 +87,8 @@ var tags = []struct{ version, buildTags string }{
 	{"10.0.0", "go1.10,!go1.13"},
 	{"11.0.0", "go1.13,!go1.14"},
 	{"12.0.0", "go1.14,!go1.16"},
-	{"13.0.0", "go1.16"},
+	{"13.0.0", "go1.16,!go1.21"},
+	{"15.0.0", "go1.21"},
 }
 
 // buildTags reports the build tags used for the current Unicode version.
@@ -276,6 +277,13 @@ func fileToPattern(filename string) string {
 	return fmt.Sprint(prefix, "%s", suffix)
 }
 
+// tagLines returns the //go:build and // +build lines to add to the file.
+func tagLines(tags string) string {
+	newTags := strings.ReplaceAll(tags, ",", " && ")
+	return "//go:build " + newTags + "\n" +
+		"// +build " + tags + "\n"
+}
+
 func updateBuildTags(pattern string) {
 	for _, t := range tags {
 		oldFile := fmt.Sprintf(pattern, t.version)
@@ -283,8 +291,7 @@ func updateBuildTags(pattern string) {
 		if err != nil {
 			continue
 		}
-		build := fmt.Sprintf("// +build %s", t.buildTags)
-		b = regexp.MustCompile(`// \+build .*`).ReplaceAll(b, []byte(build))
+		b = regexp.MustCompile(`((//go:build|// \+build).*\n)+`).ReplaceAll(b, []byte(tagLines(t.buildTags)))
 		err = os.WriteFile(oldFile, b, 0644)
 		if err != nil {
 			log.Fatal(err)
@@ -316,7 +323,8 @@ func WriteVersionedGoFile(filename, pkg string, b []byte) {
 func WriteGo(w io.Writer, pkg, tags string, b []byte) (n int, err error) {
 	src := []byte(header)
 	if tags != "" {
-		src = append(src, fmt.Sprintf("// +build %s\n\n", tags)...)
+		src = append(src, tagLines(tags)...)
+		src = append(src, '\n')
 	}
 	src = append(src, fmt.Sprintf("package %s\n\n", pkg)...)
 	src = append(src, b...)
